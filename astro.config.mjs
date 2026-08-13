52324b0edd2de59504b05b09ffc80ddcc367e065
stanley-deeplux <matdolabllc@gmail.com>
2026-08-13 09:13:11 -0400

    matdocheck tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   // Your final URL. Used for the sitemap / canonical links.
-  // This is your GitHub Pages user-site URL. Change it if you add a custom domain.
-  site: 'https://matdolabllc.github.io',
+  site: 'https://matdolab.com',
 
   vite: {
     plugins: [tailwindcss()],
